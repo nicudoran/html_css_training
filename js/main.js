ebf588aa90ca7fbb50ddd481b2866802d08e02ad
nicudoran <118466959+nicudoran@users.noreply.github.com>
2023-04-21 17:34:19 +0300

    Commit after gitigore

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-// window.addEventListener('resize',function() {
-//   if (window.matchMedia("(min-width: 768px)").matches) {
-//     console.log("Screen width is at least 500px")
-//     const resize=document.querySelectorAll(".footer__resize");
-//     resize.forEach((item) =>{
-//             console.log("textdsfjkldjvb")
-//         item.classList.remove("collapsible")
-//         item.classList.remove("collapsible--expanded")
-   
-//       })
-//     }
-//   })
-
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
 item.addEventListener("click", function () {
